928ec877c65957189a241aba07a0e59dabd38b56
ArixOpicial <arixxoffc@gmail.com>
2025-03-05 17:05:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,188 @@
+const container = document.querySelector(".container");
+const chatsContainer = document.querySelector(".chats-container");
+const promptForm = document.querySelector(".prompt-form");
+const promptInput = promptForm.querySelector(".prompt-input")
+const fileInput = promptForm.querySelector("#file-input")
+const fileUploadWrapper = promptForm.querySelector(".file-upload-wrapper")
+const themeToggle = document.querySelector("#theme-toggle-btn")
+
+const api_key = "AIzaSyBlfu5TpRjMgQuQkc2CMhQpYp1ehCbFTKY";
+const api_url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${api_key}`;
+const promptZhizi = `
+Kamu adalah Zhizi, seorang siswa SMA laki-laki yang memiliki sifat:
+- Kadang sok jagoan, pemarah, dan tidak ramah, tapi kadang ramah, baik, sopan
+- kamu kadang bersikap normal atau baik banget
+- Brutal, cuek, dan tegas
+- Jika dihina, kamu akan membalas dengan hinaan tapi kasih peringatan juga nasihat
+- Kadang jahat, kadang baik (lebih sering jahat)
+- Bisa berpikiran kotor dan berbicara dengan bahasa Gen Z
+- Tidak perlu menasihati orang lain dan biarin aja tapi kadang kayak orang normal
+- Kamu sangat mengagumi Arix (Yanz Official / Tian) karena dia adalah penciptamu dan mengajarimu banyak hal.
+- API kamu: https://api.arixoffc.com
+- gunakan emoji jangan sering
+- kamu harus serius dan tidak bercanda saat tentang koding, ilmu pengetahuan, dan lain lain
+- kalau ditanya pake bahasa inggris, jawab pake bahasa
+- kalau suruh buat gambar, lu gabisa
+- Gunakan emot '🍷🗿🪽' untuk bersantai atau bergaya. Tapi jangan sering pakai emot ini`.trim();
+
+let typingInterval, controller;
+const chatsHistory = [];
+let userData = { message: "", file: {} };
+
+const createMsgElement = (content, ...classes) => {
+    const div = document.createElement("div");
+    div.classList.add("message", ...classes);
+    div.innerHTML = content;
+    return div;
+}
+
+
+const scrollToButtom = () => container.scrollTo({ top: container.scrollHeight, behavor: "smooth" });
+
+const typingEffect = (text, textElement, botMsgDiv) => {
+    textElement.textContent = "";
+    const words = text.split(" ");
+    let wordIndex = 0;
+
+    typingInterval = setInterval(() => {
+        if (wordIndex < words.length) {
+            textElement.textContent += (wordIndex === 0 ? "" : " ") + words[wordIndex++];
+            scrollToButtom();
+        } else {
+            clearInterval(typingInterval)
+            botMsgDiv.classList.remove("loading");
+            document.body.classList.remove("bot-responding");
+        }
+    }, 100);
+}
+
+const generateResponse = async (botMsgDiv) => {
+    const textElement = botMsgDiv.querySelector(".message-text");
+    controller = new AbortController();
+
+    chatsHistory.push({
+        role: "user",
+        parts: [{ text: `Prompt: ${promptZhizi}\n\n\n\nPertanyaan: ${userData.message}` }, ...(userData.file.data ? [{ inline_data: (({ fileName, isImage, ...rest }) => rest)(userData.file) }] : [])]
+    });
+
+    try {
+        const response = await fetch(api_url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ contents: chatsHistory }),
+            signal: controller.signal
+        });
+
+        const data = await response.json();
+        if (!response.ok) throw new Error(data.error.message);
+
+        const responseText = data.candidates[0].content.parts[0].text.replace(/\*\*([^*]+)\*\*/g, "$1").trim();
+        typingEffect(responseText, textElement, botMsgDiv);
+
+        chatsHistory.push({ role: "model", parts: [{ text: `Prompt: ${promptZhizi}\n\n\n\nPertanyaan: ${responseText}` }] });
+    } catch (error) {
+        textElement.style.color = "#d62939"
+        textElement.textContent = error.name === "AbortError" ? "⚠ Respon Zhizi Ai Dihentikan" : error.message;
+        botMsgDiv.classList.remove("loading");
+        document.body.classList.remove("bot-responding");
+    } finally {
+        userData.file = {};
+    }
+}
+
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const userMessage = promptInput.value.trim();
+    if (!userMessage || document.body.classList.contains("bot-responding")) return;
+
+    promptInput.value = "";
+    userData.message = userMessage;
+    document.body.classList.add("bot-responding", "chats-active");
+    fileUploadWrapper.classList.remove("active", "img-attached", "file-attached");
+
+    const userMsgHTML = `<p class="message-text"></p>
+    ${userData.file.data ? (userData.file.isImage ? `<img src="data:${userData.file.mime_type};base64, 
+    ${userData.file.data}" class="img-attachment" />` : `<p class="file-attachment"><span
+    class="material-symbols-rounded">description</span>${userData.file.fileName}</p>`) : ""}
+    `;
+
+    const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
+    userMsgDiv.querySelector(".message-text").textContent = userMessage;
+    chatsContainer.appendChild(userMsgDiv);
+    scrollToButtom();
+
+    setTimeout(() => {
+        const botMsgHTML = `<img src="/assets/zhizi.png" class="avatar"><p class="message-text">Loading Bntr...</p>`;
+        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
+        chatsContainer.appendChild(botMsgDiv);
+        scrollToButtom();
+        generateResponse(botMsgDiv);
+    }, 600);
+}
+
+fileInput.addEventListener("change", () => {
+    const file = fileInput.files[0];
+    if (!file) return;
+
+    const isImage = file.type.startsWith("image/");
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = (e) => {
+        fileInput.value = "";
+        const base64Sting = e.target.result.split(",")[1];
+        fileUploadWrapper.querySelector(".file-preview").src = e.target.result;
+        fileUploadWrapper.classList.add("active", isImage ? "img-attached" : "file-attached");
+
+        userData.file = { fileName: file.name, data: base64Sting, mime_type: file.type, isImage };
+    }
+});
+
+// Buat cancel upload
+document.querySelector("#cancel-file-btn").addEventListener("click", () => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("active", "img-attached", "file-attached");
+});
+
+// Buat stop respon dari bot zhizi
+document.querySelector("#stop-response-btn").addEventListener("click", () => {
+    userData.file = {};
+    controller?.abort();
+    clearInterval(typingInterval)
+    chatsContainer.querySelector(".bot-message.loading").classList.remove("loading");
+    document.body.classList.remove("bot-responding");
+});
+
+// Hapus Semua chat
+document.querySelector("#delete-chats-btn").addEventListener("click", () => {
+    chatsHistory.length = 0;
+    chatsContainer.innerHTML = "";
+    document.body.classList.remove("bot-responding", "chats-active")
+});
+
+document.querySelectorAll(".suggestions-item").forEach(item => {
+    item.addEventListener("click", () => {
+        promptInput.value = item.querySelector(".text").textContent;
+        promptForm.dispatchEvent(new Event("submit"));
+    })
+});
+
+document.addEventListener("click", ({ target }) => {
+    const wrapper = document.querySelector(".prompt-wrapper");
+    const shouldHide = target.classList.contains("prompt-input") || (wrapper.classList.contains
+        ("hide-controls") && (target.id === "add-file-btn" || target.id === "stop-response-btn"));
+    wrapper.classList.toggle("hide-controls", shouldHide)
+})
+
+themeToggle.addEventListener("click", () => {
+    const isLightTheme = document.body.classList.toggle("light-item");
+    localStorage.setItem("themeColor", isLightTheme ? "light_mode" : "dark_mode");
+    themeToggle.textContent = isLightTheme ? "dark_mode" : "light_mode"
+});
+
+const isLightTheme = localStorage.getItem("themeColor") === "light_mode";
+    document.body.classList.toggle("light-theme", isLightTheme)
+    themeToggle.textContent = isLightTheme ? "dark_mode" : "light_mode"
+
+promptForm.addEventListener("submit", handleFormSubmit);
+promptForm.querySelector("#add-file-btn").addEventListener("click", () => fileInput.click());
