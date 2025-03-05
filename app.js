e0d59fdb42a649b60dc95c927628c201a5ce242e
ArixOpicial <arixxoffc@gmail.com>
2025-03-05 17:22:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,22 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route untuk halaman utama
 app.get('/', (req, res) => {
-  // Baca file script.js
-  const scriptPath = path.join(__dirname, 'script.js');
-  const scriptContent = fs.readFileSync(scriptPath, 'utf-8');
-
-  // Baca file index.html
-  const indexPath = path.join(__dirname, 'public', 'index.html');
-  let indexContent = fs.readFileSync(indexPath, 'utf-8');
-
-  // Sisipkan konten script.js sebagai inline script
-  indexContent = indexContent.replace(
-    '</body>',
-    `<script>${scriptContent}</script></body>`
-  );
-
-  // Kirim hasilnya ke klien
-  res.send(indexContent);
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Jalankan server
